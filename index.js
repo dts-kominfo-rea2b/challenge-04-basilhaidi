63c61c3359c598793c37f1e1410679bb1238df62
Basil Haidi <basilhaidi65@gmail.com>
2022-06-05 22:49:48 +0700

    modifikasi fungsi createDate

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+  '2019-01-02', // 1546387200 (epoch time, dalam detik)
+  '2022-05-06 00:00:00 UTC-2', // 1651802400 (epoch time, dalam detik)
+  '2021-03-04 07:00:00 UTC', // 1614841200 (epoch time, dalam detik)
+  '2021-04-05 00:00:00 UTC+2', // 1617573600 (epoch time, dalam detik)
+  '2020-02-03 UTC+7', // 1580662800 (epoch time, dalam detik)
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (date, index) => {
+  let arrDate = [];
+  for (let i = 0; i < date.length; i++) {
+    arrDate.push(Date.parse(date[i]) / 1000);
+  }
+
+  if (index === undefined) {
+    arrDate = arrDate.sort().join('-');
+    return arrDate.toString();
+  } else {
+    arrDate = arrDate[index];
+    return arrDate.toString();
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
